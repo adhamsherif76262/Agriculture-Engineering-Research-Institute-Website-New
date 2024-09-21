e55608ffa4a05bd14ca65695bc40590c27ea5c70
Adham Sherif <adhamsherif7261@gmail.com>
2024-09-22 00:58:37 +0300

    Finished 90% Of The Helping Units Page [English & Arabic]

--- JavaScript/Helping_Units.js
+++ JavaScript/Helping_Units.js
@@ -173,22 +173,163 @@ var Central_Laboratory_En = `
 `;
 
 var National_Laboratory_Ar = `
-  
+    <h2>المعمل القومي لاختبار مكونات شبكات الري الحقلي:</h2>
+    <p id="Overview_P">
+        يُعد المعمل القومي لاختبار مكونات شبكات الري الحقلي جزءاً أساسياً من قسم بحوث هندسة الري والصرف الحقلي في معهد
+        بحوث الهندسة الزراعية، التابع لوزارة الزراعة واستصلاح الأراضي. يهدف المعمل إلى تقديم خدمات متكاملة تختص باختبار
+        وتقييم معدات ومكونات شبكات الري وفقاً لمعايير الجودة العالمية والمحلية.
+    </p>
+    <h2>أهداف المعمل</h2>
+    <ul>
+        <li class="Services_LI">
+            <p>اختبار معدات ومكونات شبكات الري الحقلي لضمان جودتها وملاءمتها للبيئات الزراعية المختلفة.</p>
+        </li>
+        <li class="Services_LI">
+            <p>مساعدة مصممي شبكات الري في تحديد المواصفات والمكونات المثلى لأنظمة الري.</p>
+        </li>
+        <li class="Services_LI">
+            <p>تقديم دورات تدريبية متخصصة للطلاب والخريجين لتأهيلهم لسوق العمل في مجال شبكات الري.</p>
+        </li>
+    </ul>
+
+    <article>
+        <h4>الاختبارات التي يجريها المعمل:</h4>
+        <dl>
+            <div class="Types_Div">
+                <dt>الاختبارات الفيزيائية</dt>
+                <dd>تشمل قياس الأبعاد، مثل القطر والسُمك، للتأكد من مطابقة المنتجات للمواصفات.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>الاختبارات الكيميائية: </dt>
+                <dd>مثل اختبار كلوريد الميثيلين والأسيتون لاختبار جودة المواد المستخدمة ومقاومتها للعوامل البيئية.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>الاختبارات الحرارية</dt>
+                <dd>مثل اختبار التمدد الحراري لتقييم مقاومة المواد للتغيرات الحرارية.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>الاختبارات الهيدروليكية: </dt>
+                <dd>تشمل اختبار الضغط وفاقد الاحتكاك، لتحديد كفاءة الشبكات تحت ظروف العمل المختلفة.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>الاختبارات الميكانيكية:</dt>
+                <dd>مثل اختبار الصدمات لتحديد قدرة تحمل المكونات للاستخدام القاسي.</dd>
+            </div>
+        </dl>
+    </article>
+
+    <h2>الأجهزة المستخدمة في المعمل</h2>
+    <div id="Equipment_OL_Container">
+        <ol class="list">
+            <li class="item">
+                <h5 class="headline">Ultrasonic Flow Meter: </h5>
+                <span>جهاز لقياس سرعة تدفق الماء داخل الأنابيب، مما يساعد في تحديد تصرف الأنابيب ونوع السريان (رقائقي أو
+                    مضطرب).</span>
+            </li>
+            <li class="item">
+                <h5 class="headline">Burst Pressure Test Facility: </h5>
+                <span>لقياس مدى تحمل الأنابيب للضغط العالي.</span>
+            </li>
+            <li class="item">
+                <h5 class="headline">Drip Flow Rate Laboratory Test Facility: </h5>
+                <span>لاختبار معدلات التدفق في أنظمة الري بالتنقيط.</span>
+            </li>
+            <li class="item">
+                <h5 class="headline">Clogging Susceptibility for Drip Irrigation System Test Facility:</h5>
+                <span>لتقييم مدى عرضة أنظمة الري بالتنقيط للانسداد.</span>
+            </li>
+        </ol>
+    </div>
 `;
 var National_Laboratory_En = `
-  
+    <h2>The National Laboratory for Testing Field Irrigation Networks</h2>
+    <p id="Overview_P">
+        The National Laboratory for Testing Field Irrigation Networks Components is part of the Department of Irrigation
+        and Drainage Engineering at the Agricultural Engineering Research Institute, under the Ministry of Agriculture
+        and Land Reclamation. The laboratory provides comprehensive services for testing and evaluating irrigation
+        equipment and components according to international and local quality standards.
+    </p>
+    <h2>Laboratory Objectives:</h2>
+    <ul>
+        <li class="Services_LI">
+            <p>I. Testing irrigation network components to ensure their quality and suitability for various agricultural environments.</p>
+        </li>
+        <li class="Services_LI">
+            <p>II. Assisting irrigation network designers in determining optimal specifications and components for irrigation systems.</p>
+        </li>
+        <li class="Services_LI">
+            <p>III. Providing specialized training courses for students and graduates to prepare them for the job market in irrigation networks.</p>
+        </li>
+    </ul>
+
+    <article>
+        <h4>Tests Conducted by the Laboratory:</h4>
+        <dl>
+            <div class="Types_Div">
+                <dt>Physical Tests: </dt>
+                <dd>Including measurements of dimensions such as diameter and thickness to ensure compliance with
+                    standards.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>Chemical Tests: </dt>
+                <dd>Such as the Methylene Chloride and Acetone tests to assess material quality and resistance to
+                    environmental factors.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>Thermal Tests: </dt>
+                <dd>Including heat expansion tests to evaluate material resistance to thermal changes.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>Hydraulic Tests</dt>
+                <dd>Including pressure and friction loss tests to assess network efficiency under different operating
+                    conditions.</dd>
+            </div>
+            <div class="Types_Div">
+                <dt>Mechanical Tests: </dt>
+                <dd>Including impact tests to determine the durability of components under harsh conditions.</dd>
+            </div>
+        </dl>
+    </article>
+
+    <h2>Equipment Used in the Laboratory:</h2>
+    <div id="Equipment_OL_Container">
+        <ol class="list">
+            <li class="item">
+                <h5 class="headline">Ultrasonic Flow Meter: </h5>
+                <span>Measures the flow rate of water in pipes, helping determine pipe discharge and flow type (laminar
+                    or turbulent).</span>
+            </li>
+            <li class="item">
+                <h5 class="headline">Burst Pressure Test Facility: </h5>
+                <span>Measures the ability of pipes to withstand high pressure.</span>
+                
+            </li>
+            <li class="item">
+                <h5 class="headline">Drip Flow Rate Laboratory Test Facility: </h5>
+                <span>Tests the flow rates in drip irrigation systems.</span>
+            </li>
+            <li class="item">
+                <h5 class="headline">Clogging Susceptibility for Drip Irrigation System Test Facility:</h5>
+                <span>Evaluates the susceptibility of drip irrigation systems to clogging.</span>
+            </li>
+        </ol>
+    </div>
 `;
 function Helping_Units_Load_Content_Ar() {
   Helping_Units_H1.textContent = "المعمل المركزي لأجهزة القياس";
   HomePageTitle[0].innerHTML = "وحدات مساعدة";
   Central_Laboratory.innerHTML = Central_Laboratory_Ar;
   Central_Laboratory.style.direction = "rtl";
+  National_Laboratory.innerHTML = National_Laboratory_Ar;
+  National_Laboratory.style.direction = "rtl"
 }
 function Helping_Units_Load_Content_En() {
   Helping_Units_H1.textContent = "Central Laboratory for Measurement Devices";
   HomePageTitle[0].innerHTML = "AERI Helping Units Page";
   Central_Laboratory.innerHTML = Central_Laboratory_En;
   Central_Laboratory.style.direction = "ltr";
+  National_Laboratory.innerHTML = National_Laboratory_En;
+  National_Laboratory.style.direction = "ltr"
 }
 
 if (localStorage.getItem("Permenant_Language") === "English") {
